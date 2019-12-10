9b989b970f233961cc6bb866bcfdc7a89bb27f56
narek <narek_abrahamyan@epam.com>
2019-12-10 15:45:31 +0400

    set allowed origins

--- src/main/java/com/sagittarius/donations/config/WebSocketConfig.java
+++ src/main/java/com/sagittarius/donations/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/donations")
+        registry.addEndpoint("/donations").setAllowedOrigins("*")
                 .withSockJS();
     }
 
